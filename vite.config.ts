5f4730fa493f7b014770e66af6246c6244e03824
vikaandreevnadesign <vikaandreevna.k@gmail.com>
2026-05-03 00:11:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/vkstudio/',
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
